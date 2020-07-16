750e18156e05054f67254ac44f56b7fe08f5aab0
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-07-15 23:50:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+// for ([initialization]; [condition]; [iteration]) {
+//     [loop body]
+// }
+
+function writeCards(names_array, event_name) {
+    let ThankYous = []
+    for (let i = 0; i < names_array.length; i++) {
+        ThankYous.push(`Thank you, ${names_array[i]}, for the wonderful ${event_name} gift!`)
+    }
+    return ThankYous
+}
+
+// while ([condition]) {
+//     [loop body]
+// }
+
+function countDown(startfrom) {
+    while (startfrom > 0) {
+        console.log(startfrom); startfrom--;
+    }
+    console.log(startfrom)
+}
+
+// countDown(10);
